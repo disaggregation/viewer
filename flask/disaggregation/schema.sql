842be2656fa54e4377da4c0458abca48003209c6
Arne Kaas <arne.kaas@gmail.com>
2018-02-20 12:42:28 +0100

    added new log table to default db

--- flask/disaggregation/schema.sql
+++ flask/disaggregation/schema.sql
@@ -1,3 +1,22 @@
+CREATE TABLE loads (date datetime, 
+	lowtarif_demand real, 
+	hightarif_demand real, 
+	lowtarif_supply real, 
+	hightarif_supply real, 
+	demand_power real, 
+	supply_power real, 
+	gas_demand real, 
+	demand_power_L1 real, 
+	demand_power_L2 real, 
+	demand_power_L3 real, 
+	supply_power_L1 real, 
+	supply_power_L2 real, 
+	supply_power_L3 real, 
+	voltage real, 
+	current real);
+	
+CREATE VIEW loadsonly(datetime,demand) AS SELECT date,demand_power FROM loads;
+
 drop table if exists user;
 create table user (
   user_id integer primary key autoincrement,
